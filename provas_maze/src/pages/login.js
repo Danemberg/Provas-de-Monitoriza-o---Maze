2f7536bb9b195b5aa8dcd48593e66532005a2184
Rodrigo Danemberg <a44160@alunos.ipb.pt>
2021-04-14 19:42:34 +0100

    Pagina registar utilizador design

--- provas_maze/src/pages/login.js
+++ provas_maze/src/pages/login.js
@@ -7,11 +7,11 @@ function Login(){
             <div className="container login">
                 <div className="row colleft">
                     <div className="col">
-                        <h1 className="title-login" >Plataforma para geração de Provas de Monitorização com base no currículo - Maze</h1>
+                        <h1 className="titulo" >Plataforma para geração de Provas de Monitorização com base no currículo - Maze</h1>
                         <img src={Logo} height="185" width="250" />
                     </div>
                 <div className="col-6 colright">
-                        <h1 className="title-login2">Login</h1>
+                        <h1 className="titulo2">Login</h1>
                         <br></br>
                     <div className="campo">
                         <label>Email: </label>
--- provas_maze/src/pages/registar-utilizador.js
+++ provas_maze/src/pages/registar-utilizador.js
@@ -1,176 +1,97 @@
 import React from 'react'
 import RegistoService from '../app/RegistoService'
 import {withRouter} from 'react-router-dom'
-import Card from'../components/card'
 
-const estadoInicial ={        // inicio  
-    nome: '',
-    email: '',
-    senha: '',
-    entidade: '',
-    concelho: '',
-    sucesso: false,
-    errors:[],
-    atualizando : false
 
-}
+
 
 class RegistoUtilizador extends React.Component{
 
-    state = estadoInicial;
-
-    constructor(){              // obrigatoriamente quando criar um construtor chama o super 
-        super()
-        this.service = new RegistoService();
-    }
-    
-
-    onChange = (event) => {
-        const valor = event.target.value
-        const nomeDoCampo = event.target.name
-        this.setState({ [nomeDoCampo]: valor })
-    }
-
-    onSubmit =(event) => {
-        event.preventDefault(); //método que previne que o método seja executado por completo
-        const registo ={
-            nome: this.state.nome,
-            email: this.state.email,
-            senha: this.state.senha,
-            entidade: this.state.entidade,
-            concelho: this.state.concelho
-
-        }
-        try{
-            this.service.salvar(registo)
-            this.limpaCampos()
-            this.setState({sucesso:true})
-        }catch(erro){
-             const errors = erro.errors
-             this.setState({errors : errors})
-        }
-        
-    }
-
-    limpaCampos = () =>{        // limpar dados
-        this.setState(estadoInicial)
-    }
-    
-
-    componentDidMount(){
-        const email = this.props.match.params.email
-
-        if(email){
-           const resultado = this.service.obterregistos().filter( registo => registo.email === email)
-           if(resultado.length === 1){
-                const registoEncontrado = resultado[0]
-                this.setState({...registoEncontrado, atualizando: true})  // os três pontos pegam as propriedades (nome,email,preço,concelho) vai extrair e jogar dentro do state 
-           }
-        
-        }
-
-    }
     render(){
-        return(
-            <Card header={this.state.atualizando ? 'Atualização de registo ' : 'Cadastro de registo '}>          
-                    <form id="frm-registo" onSubmit={this.onSubmit}>
-                    {this.state.sucesso ? // se for sucesso vai mostrar o alerta, caso contrário não mostra nada
-                    (
-                        <div class="alert alert-dismissible alert-success">
-                        <button type="button" class="close" data-dismiss="alert">&times;</button>
-                        <strong>Bem feito!</strong> Registo realizado com sucesso!.
+        return(  
+        <form>
+            <div class="card border-danger mb-3">
+                <div class="card-header titulo">Registar:
+                    <div class="card-body">
+                    <div className="row">
+                      <div className="col-md-6">
+                        <div class="form-group row campo">
+                                <label for="exampleInputPassword1">Nome:</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control " id="exampleInputPassword1"></input>
+                            </div>
                         </div>
-                    ) : (
-                        <></>
-                    )}
-                    {this.state.errors.length > 0 &&
-
-                        this.state.errors.map(msg =>{  // para cada mensagem de erro retorna um array com o alerta de erro
-                            return(
-                                <div class="alert alert-dismissible alert-danger">
-                                    <button type="button" class="close" data-dismiss="alert">&times;</button>
-                                    <strong>Erro!</strong>{msg}.
+                            <div class="form-group row campo">
+                                    <label for="exampleInputEmail1">Email:</label>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
                                 </div>
-                            )
-                        })
-                    }
-                            
+                            </div>
+                      </div>
+                    </div>
+                        <div class="form-group row campo">
+                                <label for="exampleInputEmail1">Senha de acesso:</label>
+                            <div class="col-sm-3">
+                                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+                            </div>
+                        </div>
                         <div className="row">
-                            <div className="col-md-6">
-                                <div className="form-group">
-                                    <label>Nome: *</label>
-                                    <input  type="text" 
-                                    name="nome" 
-                                    onChange ={this.onChange}
-                                    value={this.state.nome}
-                                    className="form-control"/>
+                            <div className="col-lg-6 ">
+                                <div class="form-group row campo">
+                                      <label for="exampleSelect1">Entidade:</label>
+                                    <div class="col-sm-9">
+                                        <select class="form-control" id="exampleSelect1">
+                                        <option>Instituto Politécnico de Bragança</option>
+                                        <option>Universidade do Minho</option>
+                                        <option>Universidade do Porto</option>
+                                        <option>Universidade de Lisboa</option>
+                                        <option>Universidade de Coimbra</option>
+                                        </select>
+                                    </div>
                                 </div>
                             </div>
-
-                            <div className="col-md-6">
-                                <div className="form-group">
-                                    <label>Email: *</label>
-                                    <input type="text" 
-                                    name="email" 
-                                    disabled={this.state.atualizando}
-                                    onChange ={this.onChange} 
-                                    value={this.state.email} 
-                                    className="form-control"/>
+                                <div className="col-md-6 ">
+                                <div class="form-group row campo">
+                                        <label for="exampleInputEmail1">Outros:</label>
+                                    <div class="col-sm-9">
+                                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-
                         <div className="row">
-                            <div className="col-md-12">
-                                <div className="form-group">
-                                    <label>Senha de acesso:</label>
-                                    <input type="text" 
-                                    name="senha" 
-                                    onChange ={this.onChange} 
-                                    value={this.state.senha} 
-                                    className="form-control"/>
+                            <div className="col-lg-6 ">
+                                <div class="form-group row campo">
+                                        <label for="exampleInputEmail1">Concelho:</label>
+                                    <div class="col-sm-6">
+                                        <input type="text" readonly="" class="form-control-plaintext" id="staticEmail" value="Bragança"></input>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-6 ">
+                                <div class="form-group row campo">
+                                        <label for="exampleInputEmail1">Concelho:</label>
+                                    <div class="col-sm-6">
+                                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-
-                        <div className="row">
-                            <div className="col-md-6">
-                                <div className="form-group">
-                                    <label>Entidade: *</label>
-                                    <input type="text" 
-                                    onChange ={this.onChange} 
-                                    name="entidade" 
-                                    value={this.state.entidade}
-                                    className="form-control"/>
-                                </div>
-                            </div>
-
-                            <div className="col-md-6">
-                                <div className="form-group">
-                                    <label>Concelho: *</label>
-                                    <input type="text" 
-                                    onChange ={this.onChange} 
-                                    name="concelho" 
-                                    value={this.state.concelho} 
-                                    className="form-control"/>
-                                </div>
+                        <div class="form-group row campo">
+                                <label for="exampleTextarea">Justificação de registo:</label>
+                            <div class="col-md-6">
+                                <textarea class="form-control" id="exampleTextarea" rows="3"></textarea>
                             </div>
                         </div>
-                                               
-                        <div className="row">   
-                            <div className="col-md-1">
-                                <button type="submit" className="btn btn-success">{this.state.atualizando ? 'Atualizar' : 'Salvar'}
-                                </button> 
-                            </div>
-                            
-                            <div className="col-md-1">
-                                <button onClick={this.limpaCampos} className="btn btn-primary">Limpar</button>
-                            </div>
-                        </div>
-
-                        </form>    
-            </Card>
-
+                        <div>
+                            <button type="button" class="botao">Registar</button>
+                        </div> 
+                    </div> 
+                </div> 
+            </div>    
+        </form>
+   
+      
         )
     }
 }
