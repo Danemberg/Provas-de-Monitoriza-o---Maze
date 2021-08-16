44f6d74fca602c6f2862955032988616986f9207
Rodrigo Morais Danemberg <a44160@alunos.ipb.pt>
2021-08-16 21:52:27 +0100

    paginação

--- provas_maze/src/pages/gerir-textos.js
+++ provas_maze/src/pages/gerir-textos.js
@@ -3,10 +3,14 @@ import '../index.css';
 import { Link } from 'react-router-dom'
 import Logo from '../images/LogoMBCL.png';
 import axios from 'axios';
+import _ from 'lodash';
 
 
 function GerirTextos(){
+    const pageSize = 5;
     const [textos, setTexto] = useState([]);
+    const [paginatedTextos, setpaginatedTextos] = useState ([]);
+    const [currentPage, setcurrentPage] = useState(1)
     
       useEffect(()=>{
            loadTextos();
@@ -15,10 +19,22 @@ function GerirTextos(){
           const result = await axios.get(`http://192.168.1.84/projeto-maze/web/rest/textos`);
           console.log(result);
           setTexto(result.data)
+          setpaginatedTextos(_(result.data).slice(0).take(pageSize).value())
       }
       const deleteTexto = async id =>{
         await axios.delete(`http://192.168.1.84/projeto-maze/web/rest/textos/${id}`)
         loadTextos();
+      }
+
+      const pageCount = textos? Math.ceil(textos.length/pageSize) :0;
+      if(pageCount ===1) return null;
+      const pages = _.range(1,pageCount+1)
+
+      const pagination=(pageNo)=>{
+          setcurrentPage(pageNo);
+          const startIndex =(pageNo -1) * pageSize;
+          const paginatedTextos = _(textos).slice(startIndex).take(pageSize).value();
+          setpaginatedTextos(paginatedTextos)
       }
         return(
             <div>
@@ -63,9 +79,9 @@ function GerirTextos(){
                                 </thead>
                                 <tbody>
                                 {
-                                textos.map((texto, index) =>(
+                                paginatedTextos.map((texto) =>(
                                     <tr>
-                                        <th scope="row">{index + 1}</th>
+                                        <th scope="row"></th>
                                         <td>{texto.titulo}</td>
                                         <td>{texto.autor}</td>
                                         <td>{texto.editora_manual}</td>
@@ -80,17 +96,20 @@ function GerirTextos(){
                                                 ))}  
                                 </tbody>
                             </table>
-                            <div className="clearfix">
-                                <ul className="pagination">
-                                <a href="#" className="page-link">Anterior</a>
-                                    <a href="#" className="page-link ">1</a>
-                                    <a href="#" className="page-link ">2</a>
-                                    <a href="#" className="page-link active">3</a>
-                                    <a href="#" className="page-link">4</a>
-                                    <a href="#" className="page-link">5</a>
-                                    <a href="#" className="page-link">Próximo</a>
-                                </ul>
-                            </div>
+                           <nav>
+                               <ul className="pagination">
+                                   {
+                                       pages.map((page)=>(
+                                        <li  className={
+                                            page=== currentPage ? "page-item active " : "page-item" 
+                                        }><p className="page-link active"
+                                            onClick={()=>pagination(page)}
+                                            >{page}</p>
+                                        </li>
+                                       ))
+                                   }
+                               </ul>
+                           </nav>
                         </div>
                     </div>
             </div>
